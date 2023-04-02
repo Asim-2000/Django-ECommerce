58dfca0a92c81c5ee8bfb11adeb011c1c47555fd
Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2023-04-03 01:06:12 +0500

    📦 NEW: New Project

---
+++ playground/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+# request -> Response
+#request handler
+
+def say_hello(request):
+    x=1
+    y=2
+    return render(request,'hello.html',{'name':'Asim'})
+
